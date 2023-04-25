f05f23c91d0ae3eb143bed8eb79b8141a3244391
Pavlo <p1kutsaev@gmail.com>
2023-04-25 06:35:41 +0300

    Update home.jsx

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,13 +1,32 @@
 import { useSelector } from 'react-redux';
 import { authSelectors } from '../../redux/auth/selectors';
 
+const styles = {
+  container: {
+    minHeight: 'calc(100vh - 50px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 500,
+    fontSize: 48,
+    textAlign: 'center',
+  },
+};
+
 export default function Home() {
   // const isLoggedIn = useSelector(authSelectors.selectIsLoggedIn);
 
   return (
-    <div>
-      HOME
-      <p>Welcome to contacts manager!</p>
+    <div style={styles.container}>
+      <h1 style={styles.title}>
+        Welcome to contacts manager!{' '}
+        <span role="img" aria-label="Greeting icon">
+          📞
+        </span>
+      </h1>
+
       {/* {
       isLoggedIn ? (
         <p>Welcome to contacts manager!</p>

f8f66d4e4afe727044564d934d5b316bf6720a93
Pavlo <p1kutsaev@gmail.com>
2023-04-28 16:21:22 +0300

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,13 +2,17 @@
 import { useDispatch, useSelector } from 'react-redux';
 import css from './UserMenu.module.css';
 import { selectUsername } from 'redux/auth/selectors';
+import { logOut } from 'redux/auth/operations';
 
 export const UserMenu = () => {
+  const dispatch = useDispatch();
   const username = useSelector(selectUsername);
   return (
     <div className={css.wrapper}>
       <p className={css.username}>Welcome, {username}</p>
-      <button type="button">Logout</button>
+      <button type="button" onClick={() => dispatch(logOut())}>
+        Logout
+      </button>
     </div>
   );
 };

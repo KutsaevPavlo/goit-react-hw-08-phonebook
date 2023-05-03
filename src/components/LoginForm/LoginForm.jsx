7402f8d4dea439754267b4fc79a2e714669bf705
KutsaevPavlo <p1ul@ukr.net>
2023-05-03 23:26:33 +0300

    css

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -20,14 +20,16 @@ export const LoginForm = () => {
   return (
     <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
       <label className={css.label}>
-        Email
-        <input type="email" name="email" />
+        <div className={css.items}>Email</div>
+        <input className={css.input} type="email" name="email" />
       </label>
       <label className={css.label}>
-        Password
-        <input type="password" name="password" />
+        <div className={css.items}>Password</div>
+        <input className={css.input} type="password" name="password" />
       </label>
-      <button type="submit">Log In</button>
+      <button className={css.button} type="submit">
+        Log In
+      </button>
     </form>
   );
 };
